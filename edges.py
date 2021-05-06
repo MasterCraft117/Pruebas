91cf0b2454961025aa752f08aa73b65d99a810fe
RogerMTY <76199913+RogerMTY@users.noreply.github.com>
2021-05-06 11:33:26 -0500

    Update edges.py

--- edges.py
+++ edges.py
@@ -4,11 +4,13 @@ cap = cv2.VideoCapture(0) #0 local o primary camera
 con = 0
 while cap.isOpened():
     
-        #BGR image feed from camera
+        # Feed de la camara.
     ret, img = cap.read()
-    #BGR to grayscale
+    # Convierte la imagen a escala de grises.
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Agrega blur gaussiano.
     cv2.GaussianBlur(img_gray, (11,11), 0, img_gray)
+    # Funcion de deteccion de bordes.
     edges = cv2.Canny(img_gray,10,20,apertureSize = 3)
     
     cv2.imshow("Deteccion de bordes", edges)
