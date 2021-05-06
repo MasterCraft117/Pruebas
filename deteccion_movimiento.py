4be4d660c9a61ee4f23438bed832aa04536872ac
MACP2503 <74038341+MACP2503@users.noreply.github.com>
2021-05-06 12:41:35 -0500

    Update deteccion_movimiento.py

--- deteccion_movimiento.py
+++ deteccion_movimiento.py
@@ -2,16 +2,22 @@ import cv2
 
 cap = cv2.VideoCapture(1) #0 local o primary camera
 con = 0
+i = 0
 while cap.isOpened():
     
         #BGR image feed from camera
     ret, img = cap.read()
     #BGR to grayscale
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.GaussianBlur(img_gray, (11,11), 0, img_gray)
-    edges = cv2.Canny(img_gray,10,20,apertureSize = 3)
-    
-    cv2.imshow("Faces found", edges)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Faces found", gray)
+
+    if i == 20:
+        bgGray = gray
+    if i > 20:
+        dif = cv2.absdiff(gray, bgGray)
+        cv2.imshow('dif', dif)
+                   
+    i = i+1
     
     k = cv2.waitKey(10)
     if k==27:
