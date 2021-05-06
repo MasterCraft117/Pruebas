d5fd01a62879d82306a61040c60a86624d96faae
Rogelio <rogelioguzman73@gmail.com>
2021-05-06 15:41:41 -0500

    avance

--- contadorobjetos.py
+++ contadorobjetos.py
@@ -4,14 +4,18 @@
 
 import cv2
 
-img = cv2.imread()
+img = cv2.imread('poker.jpg')
 grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(grayscale, 100, 200)
 
-cntours = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cntours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img, cntours, -1, (0, 0, 255), 2)
+text = 'Items found: ' + str(len(cntours))
+
+cv2.putText(img, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+(255, 0, 0), 1)
 
 cv2.imshow('edges', edges)
-cv2.imshow('edges', edges)
-cv2.waitkey(0)
-cv2.destroyAllWindows
+cv2.imshow('edges', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
