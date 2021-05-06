5359f8dbdbecbd3ebae8ab318d6048ea1aa0839a
Rogelio <rogelioguzman73@gmail.com>
2021-05-06 15:09:25 -0500

    avance

--- contadorobjetos.py
+++ contadorobjetos.py
@@ -3,3 +3,9 @@
 # Referencia: https://www.youtube.com/watch?v=jYSdkLBzD88&ab_channel=OMES
 
 import cv2
+
+img = cv2.imread()
+grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(grayscale, 100, 200)
+
+_, ctns, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN)
